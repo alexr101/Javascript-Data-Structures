e0584547ef137041b8aea74f056e3fa9cfe67040
alexr101 <alexonezero@outlook.com>
2017-08-27 10:18:33 -0400

    add O(n+n) func + fix O(n^2)

--- harmlessRansomNote.js
+++ harmlessRansomNote.js
@@ -1,24 +1,55 @@
 // O(n^2)
+// Two loops
 function harmlessRansomNote(noteText, magazineText) {
-	magazineText = magazineText.split(' ');
-	noteText = noteText.split(' ');
+	magazineArr = magazineText.split(' ');
+	noteArr = noteText.split(' ');
 
-	for(var i = 0; i < magazineText.length; i++) {
-		var magazineToken = magazineText[i];
+	for(var i = 0; i < magazineArr.length; i++) {
+		var magazineWord = magazineArr[i];
 
-		for(var j = 0; j < noteText; j++) {
-			var noteToken = noteText[j];
+		for(var j = 0; j < noteArr.length; j++) {
+			var noteWord = noteArr[j];
 
-			if(magazineToken === noteText) {
-				noteText.splice(j, 1);
+			if(magazineWord === noteWord) {
+				noteArr.splice(j, 1);
 			}
 		}
 	}
 
-	if(noteText.length === 0)
+	if(noteArr.length === 0)
 		return true
 	else
 		return false;
 }
 
-var found = harmlessRansomNote(['hi you'], ['hi, this article is written just for you']);
+var found = harmlessRansomNote('hi you', 'hi this article is written just for you');
+console.log(found);
+
+
+// O(n + n)
+// Use a hashtable for constant time lookup
+function harmlessRansomNote2(noteText, magazineText) {
+	var noteArr = noteText.split(' ');
+	var magazineArr = magazineText.split(' ');
+	var magazineHT = {};
+	var found = false;
+
+	magazineArr.forEach(function(word) {
+		if(!magazineHT[word]) magazineHT[word] = 0;
+		magazineHT[word]++;
+	});
+
+	for(var i = 0; i < noteArr.length; i++) {
+		var word = noteArr[i];
+
+		if(magazineHT.hasOwnProperty(word) && magazineHT[word] > 0) 
+			magazineHT[word]--;
+		else 
+			return false;
+	}
+
+	return true;
+}
+
+var found2 = harmlessRansomNote2('hi you', 'hi this article is written just for you');
+console.log(found2);
