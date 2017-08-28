223d6fb50dd2a33743ad0a19e1afd0ab9d73f731
alexr101 <alexonezero@outlook.com>
2017-08-27 20:14:31 -0400

    work through bubble-sort

--- recursive-binary-search.js
+++ recursive-binary-search.js
@@ -1,21 +1,16 @@
 function recursiveBinarySearch(arr, searchNum) {
-	var mid = Math.floor(arr.length/2) - 1;
+	var mid = Math.floor(arr.length/2);
 	var midNum = arr[mid];
 
 	if(searchNum === midNum) return true;
 
-	if(searchNum > midNum && arr.length > 1) {
-		var splicedArr = arr.splice(midNum, arr.length)
-		return recursiveBinarySearch(splicedArr, searchNum);
-	} else if (searchNum < midNum && arr.length > 1)  {
-		var splicedArr = arr.splice(0, midNum);
-		return recursiveBinarySearch(splicedArr, searchNum);
-	} else {
+	else if(searchNum > midNum && arr.length > 1) 
+		return recursiveBinarySearch(arr.splice(mid, arr.length), searchNum);
+	else if (searchNum < midNum && arr.length > 1)
+		return recursiveBinarySearch(arr.splice(0, mid), searchNum);
+	else 
 		return false; // arr length === 1 and no match
-	}
-
 }
 
-var found = recursiveBinarySearch([1, 2, 3, 4, 5, 6], 8);
-
+var found = recursiveBinarySearch([18, 22, 31, 44, 53, 61], 18);
 console.log(found);
