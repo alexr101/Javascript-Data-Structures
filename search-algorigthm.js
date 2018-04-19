55411a9565be3d22e1c7c85bce5bc88130ba42f5
alexr101 <alexonezero@outlook.com>
2018-04-19 12:28:35 -0400

    added beginsWithTerm option to serach algorigthm

--- search-algorigthm.js
+++ search-algorigthm.js
@@ -4,11 +4,11 @@
 // This algorightm relies heavily on regex to match the right keywords
 // Can still be further configured to apply search "modes" ie case sensitive, contains any part of the search term, etc
 
-var searchArr = ['pro', 'Pro series', 'pro & vl series', 'proseries']
 // returns regex matches of the term if found in any of the words in the arr
 // options
-// caseSensitive - default false
-// globalSearch - default true
+// caseSensitive: default false
+// globalSearch: default true
+// beginsWithTerm: default false
 function search(term, arr, options) {
   
   if(!term.replace(/\s/g, '').length) // removes all white space
@@ -17,12 +17,16 @@ function search(term, arr, options) {
   var options = (options) ? options : {};
   var caseSensitive = (typeof options.caseSensitive != 'undefined') ? options.caseSensitive : false;
   var globalSearch = (typeof options.globalSearch != 'undefined') ? options.globalSearch : true;
+  var beginsWithTerm = (typeof options.beginsWithTerm != 'undefined') ? options.beginsWithTerm : false;
   var results = '';
   var regex;
   var regexSearchOptions = '';
-  var regexStr = '';
+  var regexBuilderStr = '';
+  var regexPerWord = ''
   var terms;
-
+  
+  if(beginsWithTerm)
+    regexPerWord += '^';
   if(globalSearch)
     regexSearchOptions += 'g';
   if(!caseSensitive)
@@ -30,13 +34,13 @@ function search(term, arr, options) {
 
   terms = term.match(/[^ ]+/g); // matches all non-space character sequences
   terms.map(function constructRegexTerm(term, i){
-    regexStr += '^' + term; // ^ matches words that BEGIN with term
+    regexBuilderStr += regexPerWord + term; // ^ matches words that BEGIN with term
     
     if(i !== terms.length - 1)
-      regexStr += '|';
+      regexBuilderStr += '|';
   });
   
-  regex = new RegExp(regexStr, regexSearchOptions); // gi = global (dont stop at first match) & ignore casing
+  regex = new RegExp(regexBuilderStr, regexSearchOptions); // gi = (g)lobal (dont stop at first match) & (i)gnore casing
 
   if(!globalSearch) {
     results = arr.find(function(word){
@@ -59,9 +63,10 @@ function search(term, arr, options) {
 
 var searchArr = ['pro', 'Pro series', 'pro & vl series', 'proseries']
 
-console.log( search('PRo    ', searchArr, {
-  caseSensitive: true,
-  globalSearch: true
+console.log( search('vl', searchArr, {
+  caseSensitive: false,
+  globalSearch: true,
+  beginsWithTerm: false
 }) );
 console.log( search('Pro', searchArr) );
 console.log( search('Pr', searchArr) );
@@ -71,6 +76,5 @@ console.log( search('   pro    ', searchArr) );
 console.log( search('ro', searchArr) )
 console.log( search('    ro', searchArr) )
 console.log( search('     pri', searchArr) );
-
 
 
