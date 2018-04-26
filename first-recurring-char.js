9c93475b869225e5b6ac0d2719e3984a8f08ec2d
alexr101 <alex_r_101@hotmail.com>
2018-04-26 11:21:23 -0400

    work on first-recurring-char

---
+++ first-recurring-char.js
@@ -0,0 +1,18 @@
+// Uses a simple hashmap and checks if property exists to determine recurrence
+
+var arr1 = ['a', 'b', 'c', 'a'];
+var arr2 = ['b', 'a', 'b', 'a'];
+var arr3 = ['a', 'b', 'c'];
+var arr4 = ['a', 'b', 'c', 'd', 'e', 'f', 'c'];
+
+function firstRecurringChar(arr) {
+  var map = [];
+  
+  for(var i = 0; i < arr.length; i++) {
+    if(map[arr[i]]) return arr[i];
+    map[arr[i]] = 1;
+  }
+  return null;
+}
+
+console.log( firstRecurringChar(arr4) );
