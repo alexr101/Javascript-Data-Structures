79c7d297fc812f9c98b0259e74dc8047fc94771b
Alex Padilla <padilal@padilalX3L1.local>
2018-11-12 13:52:01 -0500

    add depth first traveral & fix a couple of logic issues

--- Graphs/graph-adjacency-list.js
+++ Graphs/graph-adjacency-list.js
@@ -22,8 +22,37 @@ class Graph{
     })
   }  
 
-  traverse(node) {
-    console.log('traverse', node);
+  depthFirsttraverse(node) {
+    console.log('Depth First Traverse', node);
+    if(!this.graph[node]) return console.log('node does not exist');
+
+    let visited = [];
+    let stack = [];
+    stack.push(node);
+    while(stack.length) {
+      const currentNode = stack.pop();
+      if(visited[currentNode]) continue;
+      console.log(currentNode);
+      visited[currentNode] = true;
+
+      if(!this.graph[currentNode]) continue;
+      for(var i = 0; i < this.graph[currentNode].length; i++) {
+        const child = this.graph[currentNode][i];
+        stack.push(child);
+      }
+    }
+
+
+  }
+
+  // [1, 2]
+  // [1]
+  // 0, 2, 3, 1 
+
+  
+
+  breadthFirstTraverse(node) {
+    console.log('Breadth First Traverse', node);
     if(!this.graph[node]) return console.log('node does not exist');
 
     let visited = [];
@@ -32,17 +61,17 @@ class Graph{
 
     while(queue.length) {
       const currentNode = queue.shift();
-      
       if(visited[currentNode]) continue; // already visited
-      if(!this.graph[currentNode]) continue; // no children
+      console.log(currentNode);
+      visited[currentNode] = true;
 
+      if(!this.graph[currentNode]) continue; // no children
       for(var i = 0; i < this.graph[currentNode].length; i++) {
         const child = this.graph[currentNode][i];
-        console.log(child);
         queue.push(child);
       }
 
-      visited[currentNode] = true;
+
     }
   }
 
@@ -55,8 +84,13 @@ graph.addEdge(1, 5);
 graph.addEdge(1, 4);
 graph.addEdge(4, 3);
 graph.addEdge(4, 1);
+graph.addEdge(4, 6);
+graph.addEdge(5, 1000);
+graph.addEdge(6, 9);
+graph.addEdge(9, 12);
 // graph.addEdge(5, 4);
 
 graph.printAll();
-graph.traverse(1);
+graph.breadthFirstTraverse(1);
+graph.depthFirsttraverse(1);
 
