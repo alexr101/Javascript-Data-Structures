60a8d01a2b039197d861cfa7291980db36ddad39
Alex Padilla <padilal@padilalX3L1.local>
2018-11-12 12:16:40 -0500

    create graph traverse fn

--- Graphs/graph-adjacency-list.js
+++ Graphs/graph-adjacency-list.js
@@ -14,16 +14,38 @@ class Graph{
 
     this.graph.forEach( (list, i) => {
       if(visited[i]) return;
-
       console.log(`vertex: ${i}. Connected to:`);
       list.forEach((node) => {
         console.log(node);
       })
       visited[i] = true;
     })
+  }  
+
+  traverse(node) {
+    console.log('traverse', node);
+    if(!this.graph[node]) return console.log('node does not exist');
+
+    let visited = [];
+    let queue = [];
+    queue.push(node);
+
+    while(queue.length) {
+      const currentNode = queue.shift();
+      
+      if(visited[currentNode]) continue; // already visited
+      if(!this.graph[currentNode]) continue; // no children
+
+      for(var i = 0; i < this.graph[currentNode].length; i++) {
+        const child = this.graph[currentNode][i];
+        console.log(child);
+        queue.push(child);
+      }
+
+      visited[currentNode] = true;
+    }
   }
 
-  
 }
 
 let graph = new Graph();
@@ -33,7 +55,8 @@ graph.addEdge(1, 5);
 graph.addEdge(1, 4);
 graph.addEdge(4, 3);
 graph.addEdge(4, 1);
-graph.addEdge(5, 4);
+// graph.addEdge(5, 4);
 
 graph.printAll();
+graph.traverse(1);
 
