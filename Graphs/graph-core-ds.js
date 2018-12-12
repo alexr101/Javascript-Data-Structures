cb5f7a45cb217d589e3f07601473cb47cbaeb53b
Alex Padilla <padilal@padilalX3L1.local>
2018-12-12 11:51:18 -0500

    readjust edges DS & add all necessary edges at graph initialization

--- Graphs/graph-core-ds.js
+++ Graphs/graph-core-ds.js
@@ -1,23 +1,25 @@
 class Graph {
   constructor(edges = []){
-    this.edges = [];
+    this.edges = {};
     this.vertices = [];
 
     // build vertices from our adjacency list
-    edges.forEach((key) => {
-      this.vertices.push(key);
-    })
+    for(let vertex in edges) {
+      this.vertices.push(vertex);
+      const children = edges[vertex];
+      this.addDirectedEdge(vertex, children);
+    }
   }
 
   // Todo use same objects for edges
-  addBidirectionalEdge(vertex1, vertex2) {
+  addUndirectedEdge(vertex1, vertex2) {
     if(!this.edges[vertex1]) this.edges[vertex1] = [];
     this.edges[vertex1].push(vertex2); 
     if(!this.edges[vertex2]) this.edges[vertex2] = [];
     this.edges[vertex2].push(vertex1); 
   }
 
-  addDirectionalEdge(vertex, children = []) {
+  addDirectedEdge(vertex, children = []) {
     if(!this.edges[vertex]) this.edges[vertex] = [];
     children.forEach((child) => {
       this.edges[vertex].push(child);
@@ -28,10 +30,10 @@ class Graph {
 }
 // using this instead of nested arrays makes it
 // easier for us to insert an initial adjacency list
-let graph = new Graph([
-  { 0: [1, 2, 3] } 
-])
-graph.addBidirectionalEdge(1, 2)
-graph.addDirectionalEdge(100, [200])
+let graph = new Graph({ 
+  0: [1, 2, 3] 
+})
+graph.addUndirectedEdge(1, 2)
+graph.addDirectedEdge(100, [200])
 
 console.log(graph)
