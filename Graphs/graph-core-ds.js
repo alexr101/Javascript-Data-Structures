aa4ce32599f866c7fe6542086ba3ee6cef23e1c0
Alex Padilla <padilal@padilalX3L1.local>
2018-12-12 17:08:11 -0500

    attempt to write a shortestPath for graph

--- Graphs/graph-core-ds.js
+++ Graphs/graph-core-ds.js
@@ -51,7 +51,6 @@ class Graph {
       if(visited[current]) continue;
       visited[current] = true;
       console.log(current);
-
       if(!children) continue;
 
       children.forEach((child) => {
@@ -61,22 +60,79 @@ class Graph {
     }
   }
 
+  // Need a heap
+  // Use Dijkstra's algorithm
+  shortestPath(vertex, queryVertex) {
+    let queue = [vertex];
+    let visited = [];
+    let parents = {};
+    let path = [];
+    let found = false;
+
+    while(queue.length) {
+      const current = queue.shift();
+      const children = this.edges[current];
+
+      if(current === queryVertex) { 
+        found = true;
+        break;
+      }
+      if(visited[current]) continue;
+      visited[current] = true;
+
+      if(!children) continue;
+
+      children.forEach((child) => {
+        let parent = current;
+        if(!visited[child]) {
+          // keep track of parents
+          if(!parents[child]) parents[child] = [];
+          parents[child].push(parent);
+
+          queue.push(child);
+        }
+      })
+    }
+    console.log(parents)
+    if(found) {
+      let current = queryVertex;
+
+      while(typeof current !== 'undefined') {
+        path.unshift(current);
+        current = parents[current];
+
+      }
+    }
+
+    return path;
+  }
+
+  
+
 
 }
 // using this instead of nested arrays makes it
 // easier for us to insert an initial adjacency list
 let graph = new Graph({ 
+  99: [0],
   0: [1, 2, 3] 
 })
 graph.addUndirectedEdge(1, 2)
 graph.addDirectedEdge(100, [200])
 graph.addDirectedEdge(3, [100])
+graph.addDirectedEdge(2, [4])
 console.log('\nDFS');
 graph.depthFirstTraversal(0);
-console.log('\nBFS');
 
+console.log('\nBFS');
 graph.breadthFirstTraversal(0);
 
+console.log('\nShortest Path');
+let shortestPath = graph.shortestPath(99, 4);
+console.log(shortestPath);
+
+
+console.log('\nGRAPH')
 console.log(graph)
 
-queue = [1, 2, 3, 2]
+queue = [1, 2, 3, 2];
