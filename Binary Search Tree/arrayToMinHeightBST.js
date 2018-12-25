6bcb4e88bab5470ceb0540fb4ca1f620a34888b5
alexr101 <alex_r_101@hotmail.com>
2018-12-25 12:50:56 -0500

    complete arrayToMinHeightBST algorightm

---
+++ Binary Search Tree/arrayToMinHeightBST.js
@@ -0,0 +1,27 @@
+class BST {
+    constructor(val){
+        this.val = val;
+        this.left;
+        this.right;
+    }
+}
+
+const arrayToBST = (a) => {
+    if(!a) return null;
+    if(!a.length) return new BST(a[0])
+
+    const mid = Math.floor(a.length/2);
+    const left = arrayToBST(a.slice(0, mid));
+    const right = arrayToBST(a.slice(mid+1, a.length));
+
+    const bst = new BST(a[mid]);
+    bst.left = left;
+    bst.right = right;
+
+    return bst;
+}
+
+const bst = arrayToBST([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+console.log(bst);
+
+
