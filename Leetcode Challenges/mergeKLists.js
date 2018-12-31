81fddc0601054573eff379b5ec491e230f765d84
alexr101 <alex_r_101@hotmail.com>
2018-12-31 14:57:56 -0500

    mergeKLists

--- Leetcode Challenges/mergeKLists.js
+++ Leetcode Challenges/mergeKLists.js
@@ -45,27 +45,60 @@ class ListNode {
 };
 
 // Compare the head of all linked lists
+// Keep an array of currents
+// keep min current
 const mergeKLists2 = (lists) => {
+    let heads = [];
+    // k runtime
+    for(let i = 0; i < lists.length; i++) {
+        heads.push(lists[i]);
+    }
 
+    // 
+    let newList = new ListNode();
+    let current = newList;
+    let listCount = lists.length;
+    
+    while(listCount) {
+        let min = Number.MAX_SAFE_INTEGER;
+        let minHeadIndex = Number.MAX_SAFE_INTEGER;
+        
+        // (k/k)n / (k-1/k)n / (k-2/k)n
+        for(let i = 0; i < heads.length; i++) {
+            if(heads[i] && heads[i].val < min) {
+                minHeadIndex = i;
+                min = heads[i].val;
+            }
+        }
+        heads[minHeadIndex] = heads[minHeadIndex].next;
+        if(!heads[minHeadIndex]) listCount--;
+
+        if(!current.val) current.val = min
+        else {
+            current.next = new ListNode(min);
+            current = current.next;
+        }
+     }
+     return newList;
 }
 
-const l1 = new ListNode(5);
-l1.add(4)
+const l1 = new ListNode(1);
 l1.add(2)
-l1.add(12)
-l1.add(1)
+l1.add(3)
+l1.add(4)
+l1.add(99)
 
-const l2 = new ListNode(15);
-l2.add(41)
-l2.add(22)
-l2.add(112)
-l2.add(31)
+const l2 = new ListNode(8);
+l2.add(9)
+l2.add(10)
+l2.add(11)
+l2.add(1200)
 
-const l3 = new ListNode(95);
-l3.add(13)
-l3.add(51)
-l3.add(12)
-l3.add(8)
+const l3 = new ListNode(15);
+l3.add(17)
+l3.add(199)
+l3.add(200)
+l3.add(201)
 
-const result = mergeKLists([])
+const result = mergeKLists2([l1, l2, l3])
 console.log(result);
